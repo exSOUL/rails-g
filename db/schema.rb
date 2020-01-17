59f870c36f46f0344552f805071d008da83cbf45
exSOUL <ex.soul.1984.cosmos@gmail.com>
2020-01-18 00:22:08 +0900

    update: コメント機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_111443) do
+ActiveRecord::Schema.define(version: 2020_01_17_151349) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
